da08002fdd1f0d74c4d3f3694fdda753868315c4
Gregory Brown <gregory.brown23@gmail.com>
2021-03-06 21:15:16 -0500

    add app.py

---
+++ app.py
@@ -0,0 +1,129 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import numpy as np\n",
+    "import re\n",
+    "import datetime as dt\n",
+    "import pandas as pd"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import sqlalchemy\n",
+    "from sqlalchemy.ext.automap import automap_base\n",
+    "from sqlalchemy.orm import Session\n",
+    "from sqlalchemy import create_engine, func\n",
+    "from sqlalchemy.sql import exists"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from flask import Flask, jsonify"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#set up database\n",
+    "engine = create_engine(\"sqlite:///Resources/hawaii.sqlite\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#reflect db into new model\n",
+    "Base = automap_base()\n",
+    "#reflect tables\n",
+    "Base.prepare(engine, reflect = True)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#save reference to the tables\n",
+    "Measurement = Base.classes.measurement\n",
+    "Station = Base.classes.station"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#set up Flask\n",
+    "app = Flask(__name__)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#set up routes\n",
+    "@app.route(\"/\")\n",
+    "def welcome():\n",
+    "    \"\"\"list all routes.\"\"\"\n",
+    "    return(\n",
+    "        f\"Available Routes:<br/>)\"\n",
+    "        f\"/api/v1.0/precipitation<br/>\"\n",
+    "        f\"/api/v1.0/stations<br/>\"\n",
+    "        f\"/api/v1.0/tobs<br/>\"\n",
+    "        f\"/api/v1.0/start (enter as YYYY-MM-DD)<br/>\"\n",
+    "        f\"api.v1.0/start/end (enter as YYYY-MM-DD/YYYY-MM-DD)\"\n",
+    "    )"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
